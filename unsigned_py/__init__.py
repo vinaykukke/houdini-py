bc9ea4c169c59c7bb7c0ab918286ac219875d306
vinay kukke <kukke06@gmail.com>
2023-10-03 19:04:56 +0530

    Small updates

--- unsigned_py/__init__.py
+++ unsigned_py/__init__.py
@@ -26,20 +26,13 @@ def getAllNodes():
   print(Counter(nodes))
 
 def createSphere(**kwargs) -> None:
-    # Create a sphere with optional keyword arguments
-    sphere: hou.Node = hou.node("/obj").createNode("geo").createNode("sphere")
-    sphere.parm("type").set(2);
+  # Create a sphere with optional keyword arguments
+  sphere: hou.Node = hou.node("/obj").createNode("geo").createNode("sphere")
+  sphere.parm("type").set(2);
 
-    # Set various properties based on kwargs
-    if 'radius' in kwargs:
-        sphere.parmTuple("rad").set((2,2,2))
-        sphere.parm("rows").set(20)
-        sphere.parm("cols").set(20)
-    if 'divs' in kwargs:
-        sphere.parm("divs").set(kwargs['divs'])
+  # Set various properties based on kwargs
+  if 'radius' in kwargs:
+    sphere.parmTuple("rad").set((2,2,2))
+    sphere.parm("rows").set(20)
+    sphere.parm("cols").set(20)
     
-    # Other operations...
-    return sphere
-
-# # Example usage:
-# createSphere(radius=2.0, divs=32)
